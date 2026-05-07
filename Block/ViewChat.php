f04624018963a8fa18ac7f7730f056cb7bcc5d41
José Cruz Chávez <josecruzchavez10@gmail.com>
2026-05-06 23:55:21 -0600

    refactor: inject Parser utility and rename helper dependency in ViewChat
    - Add GDW\Core\Util\Parser to constructor injection
    - Rename $gdwHelper to $helperData for clarity
    - Replace getArrayTextarea() with Parser::textareaToArray() for exclude list parsing

--- Block/ViewChat.php
+++ Block/ViewChat.php
@@ -2,37 +2,40 @@
 namespace GDW\WidgetZendesk\Block;
 
 use GDW\Core\Helper\Data;
+use GDW\Core\Util\Parser;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 
 class ViewChat extends Template
 {
     protected $context;
-    protected $gdwHelper; 
+    protected $parser;
+    protected $helperData;
     
-    public function __construct(Data $gdwHelper, Context $context, array $data = [])
+    public function __construct(Parser $parser, Data $helperData, Context $context, array $data = [])
     {
         parent::__construct($context, $data);
-        $this->gdwHelper = $gdwHelper;
+        $this->parser = $parser;
+        $this->helperData = $helperData;
     }
 
     public function getKey()
     {
-        return $this->gdwHelper->getConfigValue('gdw/seo_zendesk/key') ?? '';
+        return $this->helperData->getConfigValue('gdw/seo_zendesk/key') ?? '';
     }
 
     public function getDelay()
     {
-        return $this->gdwHelper->getConfigValue('gdw/seo_zendesk/delay_time') ?? 0;
+        return $this->helperData->getConfigValue('gdw/seo_zendesk/delay_time') ?? 0;
     }
 
     public function getEnableCheckout()
     {
-        return $this->gdwHelper->getConfigValue('gdw/seo_zendesk/enable_checkout') ?? 0;
+        return $this->helperData->getConfigValue('gdw/seo_zendesk/enable_checkout') ?? 0;
     }
 
     public function getExcludeLoad()
     {
-        return $this->gdwHelper->getArrayTextarea($this->gdwHelper->getConfigValue('gdw/seo_zendesk/exclude_extraclass') ?? '');
+        return $this->parser->textareaToArray($this->helperData->getConfigValue('gdw/seo_zendesk/exclude_extraclass') ?? '');
     } 
 }
