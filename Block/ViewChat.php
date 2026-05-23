e4cd3882a96818513fb176ed6aa4b4d4ab9e7a23
José Cruz Chávez <josecruzchavez10@gmail.com>
2026-05-23 04:30:12 -0600

    refactor: improve phpstan typing in widget zendesk block

--- Block/ViewChat.php
+++ Block/ViewChat.php
@@ -8,10 +8,12 @@ use Magento\Framework\View\Element\Template\Context;
 
 class ViewChat extends Template
 {
-    protected $context;
-    protected $parser;
-    protected $helperData;
+    protected Parser $parser;
+    protected Data $helperData;
     
+    /**
+     * @param array<string, mixed> $data
+     */
     public function __construct(Parser $parser, Data $helperData, Context $context, array $data = [])
     {
         parent::__construct($context, $data);
@@ -19,23 +21,47 @@ class ViewChat extends Template
         $this->helperData = $helperData;
     }
 
-    public function getKey()
+    public function getKey(): string
     {
-        return $this->helperData->getConfigValue('gdw/seo_zendesk/key') ?? '';
+        return $this->normalizeString($this->helperData->getConfigValue('gdw/seo_zendesk/key') ?? '');
     }
 
-    public function getDelay()
+    public function getDelay(): int
     {
-        return $this->helperData->getConfigValue('gdw/seo_zendesk/delay_time') ?? 0;
+        return $this->toInt($this->helperData->getConfigValue('gdw/seo_zendesk/delay_time') ?? 0);
     }
 
-    public function getEnableCheckout()
+    public function getEnableCheckout(): int
     {
-        return $this->helperData->getConfigValue('gdw/seo_zendesk/enable_checkout') ?? 0;
+        return $this->toInt($this->helperData->getConfigValue('gdw/seo_zendesk/enable_checkout') ?? 0);
     }
 
-    public function getExcludeLoad()
+    /**
+     * @return array<int, string>
+     */
+    public function getExcludeLoad(): array
     {
-        return $this->parser->textareaToArray($this->helperData->getConfigValue('gdw/seo_zendesk/exclude_extraclass') ?? '');
-    } 
+        $value = $this->helperData->getConfigValue('gdw/seo_zendesk/exclude_extraclass') ?? '';
+        $excludeLoad = $this->parser->textareaToArray($this->normalizeString($value));
+
+        return array_values(array_filter($excludeLoad, static fn (mixed $item): bool => is_string($item)));
+    }
+
+    private function normalizeString(mixed $value): string
+    {
+        return is_scalar($value) ? (string) $value : '';
+    }
+
+    private function toInt(mixed $value): int
+    {
+        if (is_int($value)) {
+            return $value;
+        }
+
+        if (is_float($value) || (is_string($value) && is_numeric($value))) {
+            return (int) $value;
+        }
+
+        return 0;
+    }
 }
